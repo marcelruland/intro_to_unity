94c5b9b58730b0111546f4224c1ad54a7d62cb34
Marcel Ruland <info@marcelruland.com>
2021-03-28 20:13:06 +0200

    naming convention

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,39 +7,41 @@ public class Player : MonoBehaviour
 
     // declare player fields
     private bool isAirborn;
-    private bool jumpKeyPressed;
-    private float horizontalInput;
-    private float verticalInput;
-    private Rigidbody rigidbodyComponent;
+    private bool _jumpKeyPressed;
+    private float _horizontalInput;
+    private float _verticalInput;
+    private Rigidbody _rigidbodyComponent;
+
+    private float _rotation;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbodyComponent = GetComponent<Rigidbody>();
+        _rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
 
         if (Input.GetKeyDown(KeyCode.Space))
-            jumpKeyPressed = true;
+            _jumpKeyPressed = true;
     }
 
 
     // FixedUpdate is called once every physics update
     private void FixedUpdate()
     {
-        if (jumpKeyPressed)
+        if (_jumpKeyPressed)
         {
-            rigidbodyComponent.AddForce(Vector3.up * 3, ForceMode.VelocityChange);
-            jumpKeyPressed = false;
+            _rigidbodyComponent.AddForce(Vector3.up * 3, ForceMode.VelocityChange);
+            _jumpKeyPressed = false;
         }
 
-        rigidbodyComponent.velocity = new Vector3(rigidbodyComponent.velocity.x, rigidbodyComponent.velocity.y, verticalInput);
+        _rigidbodyComponent.velocity = new Vector3(_rigidbodyComponent.velocity.x, _rigidbodyComponent.velocity.y, _verticalInput);
     }
 }
