3a34bbce90803fd571b772c7f551a7d6dafae59d
Marcel Ruland <info@marcelruland.com>
2021-03-28 15:49:19 +0200

    add jump with Spacebar

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // declare player fields
+    private float horizontalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 3, ForceMode.VelocityChange);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+
+    // FixedUpdate is called once every physics update
+    private void FixedUpdate()
+    {
+        // bla
+    }
+}
