f191d084273b6c9571665a46a80853c782999dac
Marcel Ruland <info@marcelruland.com>
2021-03-30 17:54:59 +0200

    add throwable und hoardable

---
+++ Assets/Scripts/Hoardable.cs
@@ -0,0 +1,24 @@
+/*
+ * Apply to objects that may be hoarded.
+ * Subset of throwables.
+ * I.e.: Any item that can be hoarded can also bee thrown, but not vice versa!
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hord : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,24 @@
+/*
+ * Apply to objects that may be thrown.
+ * Superset of hoardables.
+ * I.e.: Some but not all items that can be thrown can also be hoarded.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
