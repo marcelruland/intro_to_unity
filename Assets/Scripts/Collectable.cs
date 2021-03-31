f46479aeeb6609a5ab482ce773e46d703b7ad77b
Marcel Ruland <info@marcelruland.com>
2021-03-31 09:27:13 +0200

    fix typo

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -8,7 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Throwable : MonoBehaviour
+public class Collectable : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
