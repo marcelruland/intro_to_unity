8816a6eb3a6ac6373c490c0c1a26f01714c56100
Marcel Ruland <info@marcelruland.com>
2021-03-31 11:28:03 +0200

    assign correct actions to objects

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -1,19 +1,59 @@
 /*
- * Apply to objects that may be thrown.
- * Superset of hoardables.
- * I.e.: Some but not all items that can be thrown can also be hoarded.
+ * Collectables are objects the player may pick up and then perform an action
+ * with.
  */
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    // create a list of actions that can be performed with this collectabl
+    public List<string> actions = new List<string>();
+
+    private void Awake()
+    {
+        // all collectables can be thrown
+        actions.Add("throw");
+
+        /*
+         * for every action, create an array of the TAGS of tha collectables
+         * that the action can be performed with
+         */
+        string[] hordables = new string[] {
+            "Banana",
+            "Disinfectant",
+            "Flour",
+            "ToiletRoll",
+            "Yeast",
+        };
+        string[] drinkables = new string[]
+        {
+            "Disinfectant",
+        };
+
+        /*
+         * Slightly complicated looking, but it's fast. Essentially means
+         * "Is this object in the array?"
+         * Check this for every action and if the object is in the array,
+         * then it may perform this action.
+         */
+        if (Array.Exists(hordables, element => element == gameObject.tag))
+        {
+            actions.Add("hoard");
+        }
+        if (Array.Exists(drinkables, element => element == gameObject.tag))
+        {
+            actions.Add("drink");
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
